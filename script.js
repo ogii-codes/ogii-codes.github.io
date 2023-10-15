bf4fea9bcbee0b1d5bf7eb7e9d3acd4ce3a710f0
ogii-codes <143994666+ogii-codes@users.noreply.github.com>
2023-10-15 01:03:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("ageForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var age = parseInt(document.getElementById("age").value);
+    var gender = document.getElementById("gender").value;
+    var lifeExpectancy = getLifeExpectancy(gender);
+    var summersLeft = lifeExpectancy - age;
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "<p>Based on the average life expectancy of your gender, you have about " + summersLeft + " summers left on earth.   <br><br>The 'Present' is a gift from God to be the change you want to see in the world!</p>";
+  });
+  
+  function getLifeExpectancy(gender) {
+    // Average life expectancy in years
+    if (gender === "male") {
+      return 76;
+    } if (gender === "female") {
+      return 81;
+    }
+  }
+  
